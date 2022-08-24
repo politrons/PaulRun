65f0994d37ef49be29c1ab59f2d758fbb4f428eb
politrons <politrons@gmail.com>
2022-08-25 00:47:39 +0200

    collision engine

--- src/main/scala/com/politrons/engine/HeroEngine.scala
+++ src/main/scala/com/politrons/engine/HeroEngine.scala
@@ -7,43 +7,51 @@ import java.util.concurrent.Executors
 import javax.swing.*
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * Engine responsible to show the life(heart) and remove it when the a life is lost.
- */
 class HeroEngine(var xPos: Integer,
                  var yPos: Integer) extends JLabel with ActionListener {
 
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
-  val heart = new Hero(xPos, yPos)
+  val hero = new Hero(xPos, yPos)
 
-  // - Y -> Tuple range from X
+  // - Y -> Tuple land range from X
   val collisionLand: List[Tuple2[Int, Tuple2[Int, Int]]] = List(
     new Tuple2(600, new Tuple2(0, 300)),
-    new Tuple2(340, new Tuple2(240, 350))
+    new Tuple2(340, new Tuple2(240, 350)),
+    new Tuple2(240, new Tuple2(30, 140)),
+    new Tuple2(390, new Tuple2(545, 650)),
+    new Tuple2(545, new Tuple2(490, 710)),
+    new Tuple2(595, new Tuple2(810, 1030)),
+    new Tuple2(235, new Tuple2(30, 145)),
+    new Tuple2(85, new Tuple2(245, 755)),
+    new Tuple2(545, new Tuple2(-15, 95)),
+    new Tuple2(290, new Tuple2(795, 915)),
+    new Tuple2(340, new Tuple2(915, 1030)),
+    new Tuple2(85, new Tuple2(960, 1030)),
+    new Tuple2(495, new Tuple2(650,710 ))
   )
   init()
 
   private def init(): Unit = {
     addKeyListener(new KeyListener())
     setFocusable(true)
-    setIcon(heart.imageIcon)
+    setIcon(hero.imageIcon)
     setSize(this.getPreferredSize)
-    setLocation(heart.x, heart.y)
+    setLocation(hero.x, hero.y)
     setFrameDelay()
-//    startGravity()
+    startGravity()
   }
 
   private def setFrameDelay(): Unit = {
-    val DELAY = 5
+    val DELAY = 1
     val timer = new Timer(DELAY, this)
     timer.start()
   }
 
   override def actionPerformed(e: ActionEvent): Unit = {
-    println(s"Hero X:${heart.x} Y:${heart.y}")
-    setIcon(heart.imageIcon)
-    setLocation(heart.x, heart.y)
+    println(s"Hero X:${hero.x} Y:${hero.y}")
+    setIcon(hero.imageIcon)
+    setLocation(hero.x, hero.y)
   }
 
   /**
@@ -53,10 +61,10 @@ class HeroEngine(var xPos: Integer,
     Future {
       while (true) {
         val maybeCollision = collisionLand.find(gravity => {
-          (gravity._1 == heart.y) && (heart.x >= gravity._2._1 && heart.x <= gravity._2._2)
+          (gravity._1 == hero.y) && (hero.x >= gravity._2._1 && hero.x <= gravity._2._2)
         })
         if (maybeCollision.isEmpty) {
-          heart.y += 1
+          hero.y += 1
         }
         Thread.sleep(5)
       }
@@ -72,18 +80,18 @@ class HeroEngine(var xPos: Integer,
       e.getKeyCode match {
         case KeyEvent.VK_SPACE =>
           //          imageIcon = changeImageIcon(images(s"left-" + increaseFrame))
-          heart.y -= 50
+          hero.y -= 50
         case KeyEvent.VK_LEFT =>
-          heart.x -= 30
+          hero.x -= 20
         //          imageIcon = changeImageIcon(images(s"left-" + increaseFrame))
         case KeyEvent.VK_RIGHT =>
-          heart.x += 30
+          hero.x += 20
         //          imageIcon = changeImageIcon(images(s"left-" + increaseFrame))
         case KeyEvent.VK_UP =>
-          heart.y -= 5
+          hero.y -= 10
         //          imageIcon = changeImageIcon(images(s"up-" + increaseFrame))
         case KeyEvent.VK_DOWN =>
-          heart.y += 5
+          hero.y += 10
         //          imageIcon = changeImageIcon(images(s"down-" + increaseFrame))
         case _ =>
           println("Key not implemented")
