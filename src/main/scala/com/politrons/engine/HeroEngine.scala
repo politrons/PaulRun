e2918d56b9dbb506797f0a88e0d1cec89cbe2354
politrons <politrons@gmail.com>
2022-08-25 01:59:47 +0200

    add hero sprite and movements

--- src/main/scala/com/politrons/engine/HeroEngine.scala
+++ src/main/scala/com/politrons/engine/HeroEngine.scala
@@ -6,6 +6,7 @@ import com.politrons.sprite.Hero
 import java.awt.event.{ActionEvent, ActionListener, KeyAdapter, KeyEvent}
 import java.util.concurrent.Executors
 import javax.swing.*
+import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 
 class HeroEngine(var xPos: Integer,
@@ -19,19 +20,19 @@ class HeroEngine(var xPos: Integer,
 
   // - Y -> Tuple land range from X
   val collisionLand: List[Tuple2[Int, Tuple2[Int, Int]]] = List(
-    new Tuple2(600, new Tuple2(0, 300)),
-    new Tuple2(340, new Tuple2(240, 350)),
-    new Tuple2(240, new Tuple2(30, 140)),
-    new Tuple2(390, new Tuple2(545, 650)),
-    new Tuple2(545, new Tuple2(490, 710)),
-    new Tuple2(595, new Tuple2(810, 1030)),
-    new Tuple2(235, new Tuple2(30, 145)),
-    new Tuple2(85, new Tuple2(245, 755)),
-    new Tuple2(545, new Tuple2(-15, 95)),
-    new Tuple2(290, new Tuple2(795, 915)),
-    new Tuple2(340, new Tuple2(915, 1030)),
-    new Tuple2(85, new Tuple2(960, 1030)),
-    new Tuple2(495, new Tuple2(650, 710))
+    new Tuple2(575, new Tuple2(0, 300)),
+    new Tuple2(320, new Tuple2(240, 350)),
+    new Tuple2(217, new Tuple2(30, 140)),
+    new Tuple2(370, new Tuple2(545, 650)),
+    new Tuple2(525, new Tuple2(490, 710)),
+    new Tuple2(575, new Tuple2(810, 1030)),
+    new Tuple2(217, new Tuple2(30, 145)),
+    new Tuple2(62, new Tuple2(245, 755)),
+    new Tuple2(525, new Tuple2(-15, 95)),
+    new Tuple2(267, new Tuple2(795, 915)),
+    new Tuple2(315, new Tuple2(915, 1030)),
+    new Tuple2(60, new Tuple2(960, 1030)),
+    new Tuple2(472, new Tuple2(650, 710))
   )
 
   val collisionBridge: List[Tuple2[Int, Tuple2[Int, Int]]] = List(
@@ -92,27 +93,40 @@ class HeroEngine(var xPos: Integer,
    */
   private class KeyListener() extends KeyAdapter {
 
+    private val pressedKeys = new mutable.HashSet[Int]()
+
     override def keyPressed(e: KeyEvent): Unit = {
-      e.getKeyCode match {
-        case KeyEvent.VK_SPACE =>
-//          hero.imageIcon = changeImageIcon(hero.images(s"left-" + increaseFrame()))
-          hero.y -= 50
-        case KeyEvent.VK_LEFT =>
-          hero.x -= 20
-          hero.imageIcon = changeImageIcon(hero.images(s"left-" + increaseFrame()))
-        case KeyEvent.VK_RIGHT =>
-          hero.x += 20
-          hero.imageIcon = changeImageIcon(hero.images(s"right-" + increaseFrame()))
-//        case KeyEvent.VK_UP =>
-//          hero.y -= 10
-//          hero.imageIcon = changeImageIcon(hero.images(s"up-" + increaseFrame()))
-//        case KeyEvent.VK_DOWN =>
-//          hero.y += 10
-//          hero.imageIcon = changeImageIcon(hero.images(s"down-" + increaseFrame()))
-        case _ =>
-          println("Key not implemented")
+      pressedKeys.add(e.getKeyCode)
+      if (pressedKeys.contains(KeyEvent.VK_LEFT) && pressedKeys.contains(KeyEvent.VK_SPACE)) {
+        hero.y -= 100
+        hero.x -= 100
+      } else {
+        e.getKeyCode match {
+          case KeyEvent.VK_SPACE =>
+            //          hero.imageIcon = changeImageIcon(hero.images(s"left-" + increaseFrame()))
+            hero.y -= 100
+          case KeyEvent.VK_LEFT =>
+            hero.x -= 20
+            hero.imageIcon = changeImageIcon(hero.images(s"left-" + increaseFrame()))
+          case KeyEvent.VK_RIGHT =>
+            hero.x += 20
+            hero.imageIcon = changeImageIcon(hero.images(s"right-" + increaseFrame()))
+          case KeyEvent.VK_UP =>
+            hero.y -= 10
+          //          hero.imageIcon = changeImageIcon(hero.images(s"up-" + increaseFrame()))
+          case KeyEvent.VK_DOWN =>
+            hero.y += 10
+          //          hero.imageIcon = changeImageIcon(hero.images(s"down-" + increaseFrame()))
+          case _ =>
+            println("Key not implemented")
+        }
       }
     }
+
+    override def keyReleased(e: KeyEvent): Unit = {
+      pressedKeys.remove(e.getKeyCode)
+    }
+
   }
 
 }
